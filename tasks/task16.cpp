ad4a96d4fac71b9611f350c5d116077823660ca3
Michael Lang <langm@gmx.at>
2020-12-04 06:59:34 +0100

    Prepare files for all days

---
+++ tasks/task16.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+
+#include "../library/io/files.h"
+
+int main() {
+    auto data = readFile("../data/day16_input");
+
+    long answer = 0;
+
+    std::cout << "Answer: " << answer << std::endl;
+
+    return 0;
+}
